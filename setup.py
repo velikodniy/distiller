337bc70b57c3c012d45d5741c11f4c8471d95e15
Vadim Velikodniy <vadim@velikodniy.name>
2018-09-11 13:55:04 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='distiller',
+    version='0.0',
+    description='Neural Network Distiller by Intel AI Lab',
+    author='Neta Zmora and Guy Jacob and Gal Novik',
+    url='https://github.com/NervanaSystems/distiller',
+    packages=['distiller', 'apputils'],
+    license='Apache License 2.0',
+)
